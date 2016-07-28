30f444c562f1084c80b3cfc7f2f577e00086bcde
harpender <harpender@gmail.com>
2016-07-28 19:43:32 +0100

    added initial boot code

---
+++ src/main/java/com/rockysingh/cache/SpringBootEhcacheExampleApplication.java
@@ -0,0 +1,12 @@
+package com.rockysingh.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEhcacheExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEhcacheExampleApplication.class, args);
+	}
+}
